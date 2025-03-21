cf5543faf41bfc7bb2c6207f2ef5a27455e7ecbb
Jones <joneschakk@gmail.com>
2025-03-21 13:02:55 +0100

    re-purposed filesrc

--- include/gnuradio/tddModules/tddFileSource.h
+++ include/gnuradio/tddModules/tddFileSource.h
@@ -23,16 +23,58 @@ class TDDMODULES_API tddFileSource : virtual public gr::sync_block {
 public:
   typedef std::shared_ptr<tddFileSource> sptr;
 
-  /*!
-   * \brief Return a shared_ptr to a new instance of tddModules::tddFileSource.
-   *
-   * To avoid accidental use of raw pointers, tddModules::tddFileSource's
-   * constructor is in a private implementation
-   * class. tddModules::tddFileSource::make is the public interface for
-   * creating new instances.
-   */
+    /*!
+     * \brief Create a file source.
+     *
+     * Opens \p filename as a source of items into a flowgraph. The
+     * data is expected to be in binary format, item after item. The
+     * \p itemsize of the block determines the conversion from bits
+     * to items. The first \p offset items (default 0) will be
+     * skipped.
+     *
+     * If \p repeat is turned on, the file will repeat the file after
+     * it's reached the end.
+     *
+     * If \p len is non-zero, only items (offset, offset+len) will
+     * be produced.
+     *
+     * \param itemsize        the size of each item in the file, in bytes
+     * \param filename        name of the file to source from
+     * \param repeat  repeat file from start
+     * \param offset  begin this many items into file
+     * \param len     produce only items (offset, offset+len)
+     */
   static sptr make(size_t itemsize, const char *filename, bool repeat = false,
                    uint64_t offset = 0, uint64_t len = 0);
+
+                   /*!
+     * \brief seek file to \p seek_point relative to \p whence
+     *
+     * \param seek_point      sample offset in file
+     * \param whence  one of SEEK_SET, SEEK_CUR, SEEK_END (man fseek)
+     */
+    virtual bool seek(int64_t seek_point, int whence) = 0;
+
+    /*!
+     * \brief Opens a new file.
+     *
+     * \param filename        name of the file to source from
+     * \param repeat  repeat file from start
+     * \param offset  begin this many items into file
+     * \param len     produce only items [offset, offset+len)
+     */
+    virtual void
+    open(const char* filename, bool repeat, uint64_t offset = 0, uint64_t len = 0) = 0;
+
+    /*!
+     * \brief Close the file handle.
+     */
+    virtual void close() = 0;
+
+    /*!
+     * \brief Add a stream tag to the first sample of the file if true
+     */
+    virtual void set_begin_tag(pmt::pmt_t val) = 0;
 };
 
 } // namespace tddModules
--- lib/tddFileSource_impl.h
+++ lib/tddFileSource_impl.h
@@ -14,8 +14,24 @@ namespace gr {
 namespace tddModules {
 
 class tddFileSource_impl : public tddFileSource {
-private:
-  // Nothing to declare in this block.
+  private:
+    const size_t d_itemsize;
+    uint64_t d_start_offset_items;
+    uint64_t d_length_items;
+    uint64_t d_items_remaining;
+    FILE* d_fp;
+    FILE* d_new_fp;
+    bool d_repeat;
+    bool d_updated;
+    bool d_file_begin;
+    bool d_seekable;
+    long d_repeat_cnt;
+    pmt::pmt_t d_add_begin_tag;
+
+    gr::thread::mutex fp_mutex;
+    pmt::pmt_t _id;
+
+    void do_update();
 
 public:
   tddFileSource_impl(size_t itemsize, const char *filename, bool repeat,
@@ -25,6 +41,16 @@ public:
   // Where all the action really happens
   int work(int noutput_items, gr_vector_const_void_star &input_items,
            gr_vector_void_star &output_items);
+
+  bool seek(int64_t seek_point, int whence) override;
+  void open(const char* filename, bool repeat, uint64_t offset, uint64_t len) override;
+  void close() override;
+
+  int work(int noutput_items,
+            gr_vector_const_void_star& input_items,
+            gr_vector_void_star& output_items) override;
+
+  void set_begin_tag(pmt::pmt_t val) override;
 };
 
 } // namespace tddModules
